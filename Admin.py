6fd28730ad7bfc6eee36d80bcd7488cfb6f3284e
Therese Ritter <98991018+taritter@users.noreply.github.com>
2024-02-02 14:41:26 -0500

    how to get data from firestore

--- Admin.py
+++ Admin.py
@@ -2,6 +2,7 @@ import firebase_admin
 import json
 from firebase_admin import credentials
 from firebase_admin import firestore
+from google.cloud.firestore_v1 import FieldFilter
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -18,10 +19,24 @@ with open("Books.json") as json_file:
         book_dict = {
             "author": book_data.get("author"),
             "genre": book_data.get("genre"),
-            "goodreads rating": book_data.get("goodreads rating"),
-            "date published": book_data.get("date published"),
+            "goodreads_rating": book_data.get("goodreads rating"),
+            "date_published": book_data.get("date published"),
             "series": book_data.get("series?"),
             "cost": book_data.get("cost (paperback)"),
-            "our rating": book_data.get("our rating (out of 5)"),
+            "our_rating": book_data.get("our rating (out of 5)"),
         }
         db.collection("Books").document(title).set(book_dict)
+
+
+# filters out all books with genre as fantasy
+fantasybooks = db.collection("Books").where(filter=FieldFilter("genre", "==", "Fantasy")).stream()
+
+for f in fantasybooks:
+    print(f"{f.id} => {f.to_dict()}")
+
+# gets information about a specific book
+title = "Crime and Punishment"
+crime = db.collection("Books").document(title).get()
+print(crime.to_dict())
+print(crime.get("cost"))
+
