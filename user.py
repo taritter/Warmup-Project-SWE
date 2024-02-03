8cd8af751b13828233b386e66da5664bea91cbe8
trueshiba <107582031+trueshiba@users.noreply.github.com>
2024-02-03 15:54:36 -0500

    user python file to read database

---
+++ user.py
@@ -0,0 +1,35 @@
+
+
+#import firebase_admin
+#from firebase_admin import credentials
+
+#cred = credentials.Certificate("path/to/serviceAccountKey.json")
+#firebase_admin.initialize_app(cred)
+
+
+ex_string1 = 'genre is "Fantasy" and genre is "Fiction"'
+
+ex_string2 = '"If you give a mouse a cookie"'
+
+ex_string3 = 'author of "Love Hypothesis"'
+
+ex_string4 = input("Show me: ")
+
+def parse(s: str):
+
+    parsed_str = ""
+
+    split1 = s.split('"')
+    print(f"Split: {split1}\nLength: {len(split1)}\n")
+
+    #if len(split1) < 
+
+    return parsed_str
+
+parse(ex_string1)
+
+parse(ex_string2)
+
+parse(ex_string3)
+
+parse(ex_string4)
