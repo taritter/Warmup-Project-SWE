5f773900d4d5bb8335e9b2449371d2fd73d82d98
trueshiba <107582031+trueshiba@users.noreply.github.com>
2024-02-14 16:22:52 -0500

    Updated user.py
    it works now maybe

--- user.py
+++ user.py
@@ -1,22 +1,26 @@
-from pyparsing import one_of, OneOrMore, ZeroOrMore, Word, Opt, Suppress, alphanums, originalTextFor
+from pyparsing import one_of, OneOrMore, ZeroOrMore, Word, Opt, Suppress, originalTextFor
 
 import Utilities
 
 from google.cloud.firestore_v1 import FieldFilter
 
+#pip install --upgrade firebase-admin
 
 # Example Strings
-# ex_string1 = 'genre == "Fantasy" and author == "Samantha Shannon"'
-# ex_string2 = 'genre == "Fantasy" and author == "Samantha Shannon" or cost < 4'
-
+ex_string1 = 'genre == "Fantasy" and author == "Samantha Shannon"'
+ex_string2 = 'genre == "Fantasy" and author == "Samantha Shannon" or cost < 4.4'
+# cost == 23.99 or author == "Samantha Shannon"
+     
 
 # Pyparsing forms
+alphanums = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.'
 field = one_of("title cost author date_published genre goodreads_rating our_rating")
 value = Suppress(Opt('\"')) + OneOrMore(Word(alphanums)) + Suppress(Opt('\"'))
 operators = one_of("== < > of")
 concat = one_of("and or")
 query_form = field + operators + originalTextFor(value)
 combined_query = query_form + ZeroOrMore(concat + query_form)
+
 
 
 # Parse function
@@ -30,7 +34,7 @@ def parse(s: str):
         if s[0] == '\"':
 
             # title query bc first char is beginning of quotes
-            title_query(s)
+            return title_query(s) 
 
         else:
             # Figure out if its 'of query' or 'regular query'
@@ -42,48 +46,31 @@ def parse(s: str):
 
             for i in result:
                 if i == "or":
-                    if temp_and_list:                          #check if there are values separated by and
-                        temp_or_list.append(temp_and_list)   #add values separated by and to or list
-                    query_array.append(temp_or_list)      #add or list to the main query array
-                    temp_and_list = []                     #reset lists
+                    if temp_and_list:  #check if there are values separated by and
+                        temp_or_list.append(temp_and_list)  #add values separated by and to or list
+                    query_array.append(temp_or_list)  #add or list to the main query array
+                    temp_and_list = []  #reset lists
                     temp_or_list = []
                 elif i == "and":
-                    if temp_and_list:                        #check if there are values separated by "and"
-                        temp_or_list.append(temp_and_list)   #add values separated by and to or list
-                    temp_and_list = []                         #reset and list
+                    if temp_and_list:  #check if there are values separated by "and"
+                        temp_or_list.append(temp_and_list)  #add values separated by and to or list
+                    temp_and_list = []  #reset and list
                 else:
                     temp_and_list.append(i)
 
-            if temp_and_list:                         #check if there are values in and list
-                temp_or_list.append(temp_and_list)      #add values to or list
-            query_array.append(temp_or_list)           #add or list to the main query array
+            if temp_and_list:  #check if there are values in and list
+                temp_or_list.append(temp_and_list)  #add values to or list
+            query_array.append(temp_or_list)  #add or list to the main query array
 
     except:
+        #
         print("failed")
 
     return query_array
 
 
-def book_title(title, db):
-    # gets information about a specific book
-    field = db.collection("Books").document(title).get()
-    # print(field.to_dict())
-    if field.to_dict() == None:
-        print("This book does not exist in the database")
-    return field.to_dict()
 
 
-def of_query(field: str, title: str):
-    # make single call to query
-    return NotImplementedError
-
-
-def operator_query(fields: list, operators: list, values: list):
-    # Each item[i] in each list corresponds to 1 query.
-    # For single querys there will only be 1 item in each list
-    # Concatenate queries together with for loop
-
-    return NotImplementedError
 def filter_fields_and(or_arr, db) -> dict:
     """
     field = "genrez"
@@ -92,17 +79,25 @@ def filter_fields_and(or_arr, db) -> dict:
     """
     books_ref = db.collection("Books")
     book_set = set()
+    # test = db.collection("Books").where(filter=FieldFilter("genre", "==", "Fantasy")).stream()
+    # for bt in test:
+    #     print(bt.id)
     books_or = {}
 
     for and_array in or_arr:
+        #print(and_array)
         books_ref = db.collection("Books")
         for statement in and_array:
 
+            print(type(statement[2]))
+
             try:
-                query_value = float(statement[2])
+                query_value = float(statement[2].strip('\"'))
             except:
                 query_value = str(statement[2]).strip("\"")
 
+            print(type(query_value))
+            print(query_value)
 
             books_ref = books_ref.where(filter=FieldFilter(statement[0], statement[1], query_value))
 
@@ -114,6 +109,8 @@ def filter_fields_and(or_arr, db) -> dict:
     return book_set
 
 
+
+
 def book_add(books_ref, book_set):
     book = books_ref.stream()
 
@@ -121,7 +118,11 @@ def book_add(books_ref, book_set):
         book_set.add(b.id)
 
 
+
+
 def main():
+
+    #print(parse(ex_string2))
 
     print("Welcome to ... \nfor help type 'help'")
 
@@ -136,19 +137,19 @@ def main():
             print("Help message...")
             continue
 
-
+        print(parse(query_prompt))
         filter_fields_and(parse(query_prompt), db)
 
         done = input("Would you like to make another query? (y/n)")
-        
-        done.casefold()
-        
-        if done == 'n' or done == 'no':
+
+        if done.casefold() == 'n':
             doneQuerying = True
 
 
 
     return 0
+
+
 
 
 if __name__ == "__main__":
